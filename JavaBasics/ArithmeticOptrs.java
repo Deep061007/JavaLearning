4a16006ee1906b69380c9d51144604795bbdc954
Deep061007 <deepbandekar01@gmail.com>
2025-03-20 13:19:55 +0530

    Update ArithmeticOptrs.java

--- JavaBasics/ArithmeticOptrs.java
+++ JavaBasics/ArithmeticOptrs.java
@@ -1,1 +1,17 @@
-
+class ArithmeticOptrs
+{
+public static void main(String[] args)
+{
+int num1=60;
+int num2=20;
+int result;
+result = num1 + num2;
+System.out.println("Addition:"+ result);
+result = num1 - num2;
+System.out.println("Substraction:"+ result);
+result = num1 * num2;
+System.out.println("Multiplication:"+ result);
+result = num1 / num2;
+System.out.println("Division:"+ result);
+}
+}
