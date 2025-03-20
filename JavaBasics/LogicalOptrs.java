862bcb2295a94bd52ad494c0d94e4b89b01c78a8
Deep061007 <deepbandekar01@gmail.com>
2025-03-20 21:29:47 +0530

    Add files via upload

---
+++ JavaBasics/LogicalOptrs.java
@@ -0,0 +1,17 @@
+class LogicalOptrs
+{
+public static void main(String [] args)
+{
+int x = 10;
+int y = 20;
+int a = 30;
+int b = 40;
+boolean result;
+result = x<y && a<b ; 
+System.out.println(result);
+result = x>y || a<b ; 
+System.out.println(result);
+result = !(a<b) ; 
+System.out.println(result);
+}
+}
