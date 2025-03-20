1c0c7f3d092be5f287bd65e75e0e6d5eeb222ab4
Deep061007 <deepbandekar01@gmail.com>
2025-03-20 13:23:08 +0530

    Add files via upload

---
+++ JavaBasics/RelationalOptrs.java
@@ -0,0 +1,17 @@
+class RelationalOptrs
+{
+public static void main(String[] args)
+{
+int a = 10;
+int b = 20;
+boolean result;
+result = a >= b;
+System.out.println("a >= b: " + result);
+result = a <= b;
+System.out.println(" a <= b: " + result);
+result = a == b;
+System.out.println("a == b:" + result);
+result = a != b;
+System.out.println(" a != b: " + result);
+}
+}
