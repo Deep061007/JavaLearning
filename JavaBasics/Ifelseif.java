560f157e2a83fbca171e76ab4c2ad1c2ff88a397
Deep <deepbandekar01@gmail.com>
2025-03-21 13:32:47 +0530

    Add files via upload

---
+++ JavaBasics/Ifelseif.java
@@ -0,0 +1,16 @@
+class Ifelseif
+{
+public static void main(String [] args)
+{
+int x = 15;
+int y = 30;
+int z = 45;
+if(x>y && x<z){
+System.out.println(x);
+}
+else if(y>x && y>z)
+System.out.println(y);
+else 
+System.out.println(z);
+}
+}
