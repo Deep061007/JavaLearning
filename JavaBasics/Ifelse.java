77b052bb9274885131d53654efa85972e32ad940
Deep061007 <deepbandekar01@gmail.com>
2025-03-21 13:01:07 +0530

    Add files via upload

---
+++ JavaBasics/Ifelse.java
@@ -0,0 +1,13 @@
+class Ifelse
+{
+public static void main(String [] args)
+{
+int x = 18;
+if(x>=18){
+System.out.println("You are legal");
+System.out.println("You are allowed for the party");
+}
+else
+System.out.println("You are not legal");
+}
+}
