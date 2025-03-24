cbd194074808e200639706ba98678b4ff3d9580a
Deep <deepbandekar01@gmail.com>
2025-03-24 16:23:56 +0530

    Add files via upload

---
+++ JavaBasics/Switchcase.java
@@ -0,0 +1,27 @@
+class Switchcase
+{
+public static void main(String [] args)
+{
+int n = 5;
+switch(n) {
+case 1:
+System.out.println("January");
+break;
+case 2:
+System.out.println("March");
+break;
+case 3:
+System.out.println("May");
+break;
+case 4:
+System.out.println("July");
+break;
+case 5:
+System.out.println("October");
+break;
+case 6:
+System.out.println("December");
+break;
+}
+}
+}
