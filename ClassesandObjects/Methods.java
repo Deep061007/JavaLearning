1c715219cf753c1c582793f9e96a19782bccfb53
Deep <deepbandekar01@gmail.com>
2025-03-31 18:54:16 +0530

    Add files via upload

---
+++ ClassesandObjects/Methods.java
@@ -0,0 +1,19 @@
+class Table {
+public void Lamp()
+{
+System.out.println("The Lamp is on");
+}
+public void pen()
+{
+System.out.println("The Pen is on the table");
+}
+}
+public class Methods
+{
+public static void main(String [] args)
+{
+Table obj = new Table();
+obj.Lamp();
+obj.pen();
+}
+}
