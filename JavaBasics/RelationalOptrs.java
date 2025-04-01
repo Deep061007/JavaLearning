f816924fe0d81f678dbca8aef078656dc01b112a
Deep <deepbandekar01@gmail.com>
2025-04-01 18:53:41 +0530

    Update RelationalOptrs.java

--- JavaBasics/RelationalOptrs.java
+++ JavaBasics/RelationalOptrs.java
@@ -2,8 +2,8 @@ class RelationalOptrs
 {
 public static void main(String[] args)
 {
-int a = 10;
-int b = 20;
+int a = 15;
+int b = 30;
 boolean result;
 result = a >= b;
 System.out.println("a >= b: " + result);
