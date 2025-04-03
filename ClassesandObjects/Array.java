6221f9a6d2e1efbbac3c410d53d7df30436351c1
Deep <deepbandekar01@gmail.com>
2025-04-03 19:29:18 +0530

    Add files via upload

---
+++ ClassesandObjects/Array.java
@@ -0,0 +1,16 @@
+class Array
+{
+public static void main(String [] args)
+{
+int num[] = new int[5];
+num [0] = 6;
+num [1] = 8;
+num [2] = 10;
+num [3] = 12;
+num [4] = 13;
+for(int i=0;i<=4;i++)
+{
+System.out.println(num[i]);
+}
+}
+}
