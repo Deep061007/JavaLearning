4c593b0715d36b8ea44794819ba978540a6dd0f7
Deep <deepbandekar01@gmail.com>
2025-04-17 19:48:05 +0530

    Update ArithmeticOptrs.java

--- JavaBasics/ArithmeticOptrs.java
+++ JavaBasics/ArithmeticOptrs.java
@@ -2,8 +2,8 @@ class ArithmeticOptrs
 {
 public static void main(String[] args)
 {
-int num1=60;
-int num2=30;
+int num1=40;
+int num2=20;
 int result;
 result = num1 + num2;
 System.out.println("Addition:"+ result);
