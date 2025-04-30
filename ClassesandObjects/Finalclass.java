626d342dd2bfd1e06e7f7b24f4c1b1243fca57d8
Deep <deepbandekar01@gmail.com>
2025-04-30 15:11:53 +0530

    Add files via upload

---
+++ ClassesandObjects/Finalclass.java
@@ -0,0 +1,15 @@
+final class Calc
+{
+public void add(int a,int b)
+{
+System.out.println(a+b);
+}
+}
+class Finalclass
+{
+public static void main(String [] args)
+{
+Calc obj = new Calc();
+obj.add(5,10);
+}
+}
